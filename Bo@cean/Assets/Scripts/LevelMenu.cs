6800d2d7854a167a6538f644a09d33d87bffed42
Z4l3s5i0 <79455975+Z4l3s5i0@users.noreply.github.com>
2026-05-19 08:29:58 +0200

    fix: Main Menu works, L1 tutorial text is on time again

--- Bo@cean/Assets/Scripts/LevelMenu.cs
+++ Bo@cean/Assets/Scripts/LevelMenu.cs
@@ -23,7 +23,7 @@ public class LevelMenu : MonoBehaviour
     }
     public void OpenLevel(int levelId)
     {
-        string levelName = "Level " + levelId;
+        string levelName = "L" + levelId;
         SceneManager.LoadScene(levelName);
     }
 
