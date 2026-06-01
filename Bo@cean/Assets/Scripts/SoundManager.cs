487b2de20766abcf7bd4f6624e2166dc48e5a8e0
susanneliskova <susanne.liskova@students.unibe.ch>
2026-06-01 15:21:28 +0200

    -wind sounds -backround music

--- Bo@cean/Assets/Scripts/SoundManager.cs
+++ Bo@cean/Assets/Scripts/SoundManager.cs
@@ -3,10 +3,25 @@ using UnityEngine;
 public class SoundManager : MonoBehaviour
 {
     public AudioSource audioSource;
+	public AudioSource musicSource;
+	public AudioClip backgroundMusic;
 
     public AudioClip rainSound;
     public AudioClip thunderSound;
     public AudioClip windSound;
+
+	void Start()
+{
+    PlayBackgroundMusic();
+}
+
+public void PlayBackgroundMusic()
+{
+    musicSource.clip = backgroundMusic;
+    musicSource.loop = true;
+   musicSource.volume = 0.02f;
+    musicSource.Play();
+}
 
     public void PlayRain()
     {
