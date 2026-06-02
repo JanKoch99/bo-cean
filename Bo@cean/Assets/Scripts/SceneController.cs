280fd519dfd6ba195a320110e7f0e4da0af2ffdc
JanKoch99 <jankoch99@bluewin.ch>
2026-06-02 21:30:34 +0200

    Finishing the finish

--- Bo@cean/Assets/Scripts/SceneController.cs
+++ Bo@cean/Assets/Scripts/SceneController.cs
@@ -7,6 +7,11 @@ public class SceneController : MonoBehaviour
     
     public static SceneController instance;
     [SerializeField] Animator tansitionAnim;
+    [SerializeField] GameObject congratulationPanel;
+    private bool showCongratulation = false;
+    private GUIStyle congratulationStyle;
+    private GUIStyle congratulationShadowStyle;
+
     private void Awake()
     {
         if (instance == null)
@@ -31,6 +36,21 @@ public class SceneController : MonoBehaviour
         SceneManager.LoadSceneAsync(sceneName);
     }
 
+    void UnlockLevel()
+    {
+        if (SceneManager.GetActiveScene().name == "L4")
+        {
+            PlayerPrefs.SetInt("UnlockedLevel", 4);
+            PlayerPrefs.Save();
+        }
+        else if (SceneManager.GetActiveScene().buildIndex >= PlayerPrefs.GetInt("ReachedIndex"))
+        {
+            PlayerPrefs.SetInt("ReachedIndex", SceneManager.GetActiveScene().buildIndex + 1);
+            PlayerPrefs.SetInt("UnlockedLevel", PlayerPrefs.GetInt("UnlockedLevel", 1) + 1);
+            PlayerPrefs.Save();
+        }
+    }
+
 
     IEnumerator LoadLevel()
     {
@@ -38,21 +58,64 @@ public class SceneController : MonoBehaviour
 
         yield return new WaitForSeconds(1);
 
-        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        UnlockLevel();
 
-        // Check if another scene exists
-        if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
+        if (SceneManager.GetActiveScene().name == "L4")
         {
-            SceneManager.LoadSceneAsync(nextSceneIndex);
+            showCongratulation = true;
+            
+            Time.timeScale = 0;
+            
+            yield return new WaitForSecondsRealtime(3); 
+            
+            showCongratulation = false;
+            Time.timeScale = 1;
+            SceneManager.LoadScene("Main Menu");
         }
         else
         {
-            Debug.Log("No more scenes. Loading Main Menu.");
+            int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
 
-            SceneManager.LoadScene("Main Menu");
+            if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
+            {
+                SceneManager.LoadSceneAsync(nextSceneIndex);
+            }
+            else
+            {
+                Debug.Log("No more scenes. Loading Main Menu.");
+
+                SceneManager.LoadScene("Main Menu");
+            }
         }
 
         tansitionAnim.SetTrigger("Start");
 
+    }
+
+    private void OnGUI()
+    {
+        if (showCongratulation)
+        {
+            if (congratulationStyle == null)
+            {
+                congratulationStyle = new GUIStyle();
+                congratulationStyle.fontSize = 50;
+                congratulationStyle.fontStyle = FontStyle.Bold;
+                congratulationStyle.normal.textColor = Color.yellow;
+                congratulationStyle.alignment = TextAnchor.MiddleCenter;
+
+                congratulationShadowStyle = new GUIStyle(congratulationStyle);
+                congratulationShadowStyle.normal.textColor = Color.black;
+            }
+
+            string text = "All Levels Completed! Congratulations!";
+            float width = Screen.width;
+            float height = 100f;
+            float x = 0;
+            float y = (Screen.height - height) * 0.5f;
+
+            GUI.Label(new Rect(x + 2, y + 2, width, height), text, congratulationShadowStyle);
+            GUI.Label(new Rect(x, y, width, height), text, congratulationStyle);
+        }
     }
 }
